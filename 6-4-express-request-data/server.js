f3b58bacba21e8aaae6336957bece55ca1e779eb
janadamdam <s202323190@kfupm.edu.sa>
2026-04-08 12:50:38 +0300

    TODO2 done

--- 6-4-express-request-data/server.js
+++ 6-4-express-request-data/server.js
@@ -115,6 +115,13 @@ app.listen(3000, () => console.log("API running at http://localhost:3000"));
 
 
 // Query params: /echo?name=Ali&age=22
+app.get("/echo", (req, res) => {
+   const { name, age } = req.query;
+   if (!name || !age) {
+      return res.status(400).json({ ok: false, error: "name & age required" });
+   }
+   res.json({ ok: true, name, age, msg: `Hello ${name}, you are ${age}` });
+});
 
 
 // Route params: /profile/First/Last
