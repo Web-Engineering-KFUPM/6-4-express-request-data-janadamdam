e2e86e6da0cbe81a48e294f9f297c3d7b470b630
janadamdam <s202323190@kfupm.edu.sa>
2026-04-08 12:56:17 +0300

    TODO5 done

--- 6-4-express-request-data/server.js
+++ 6-4-express-request-data/server.js
@@ -143,6 +143,9 @@ app.param("userId", (req, res, next, userId) => {
 
 
 // Route params: /users/:userId route
+app.get("/users/:userId", (req, res) => {
+   res.json({ ok: true, userId: req.userIdNum });
+});
 
 
 // Start the server by listening
