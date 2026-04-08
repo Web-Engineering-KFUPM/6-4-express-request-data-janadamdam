6c0677d0447542cea626689931e5980a86a2f659
janadamdam <s202323190@kfupm.edu.sa>
2026-04-08 12:54:54 +0300

    TODO4 done

--- 6-4-express-request-data/server.js
+++ 6-4-express-request-data/server.js
@@ -132,6 +132,14 @@ app.get("/profile/:first/:last", (req, res) => {
 
 
 // Route param middleware example: /users/42
+app.param("userId", (req, res, next, userId) => {
+   const userIdNum = Number(userId);
+   if (isNaN(userIdNum) || userIdNum <= 0) {
+      return res.status(400).json({ ok: false, error: "userId must be positive number" });
+   }
+   req.userIdNum = userIdNum;
+   next();
+});
 
 
 // Route params: /users/:userId route
